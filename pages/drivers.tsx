538d4d9c7372beadfeb1147e06b3226b6f023946
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2023-03-15 21:52:21 -0300

    Adiciona select com opção de consultar pilotos de outros anos da categoria

--- pages/drivers.tsx
+++ pages/drivers.tsx
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import { DriverInfo, DriversInfo } from "interfaces/interfaces";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -16,19 +17,23 @@ import Col from "react-bootstrap/Col";
 let order = "desc";
 
 const Drivers: NextPage = () => {
+  var yearSearch = new Date().getFullYear();
+  const [year, setYear] = useState(yearSearch);
   const [driverList, setDriverList] = useState(Array<DriverInfo>);
 
   useEffect(() => {
     const loadAll = async () => {
-      var actualYear = new Date().getFullYear();
-      let list = await Request.driversList(actualYear);
+      let list = await Request.driversList(year);
       let data = list[0].items;
       setDriverList(data);
     };
 
     loadAll();
-    console.log("opa");
-  }, []);
+  }, [year]);
+
+  const reloadNewYear = (newYear: any) => {
+    setYear(newYear);
+  }
 
   const orderAsc = (a: DriverInfo, b: DriverInfo) => {
     if (Number(a.permanentNumber) < Number(b.permanentNumber)) return -1;
@@ -56,6 +61,21 @@ const Drivers: NextPage = () => {
 
   return (
     <Container>
+      <Form.Select
+        value={year}
+        onChange={(e: any) => reloadNewYear(e.currentTarget.value)}
+      >
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+        <option value="2017">2017</option>
+        <option value="2016">2016</option>
+        <option value="2015">2015</option>
+        <option value="2014">2014</option>
+      </Form.Select>
+      <br />
       <Table striped bordered hover size="sm">
         <thead>
           <tr>
