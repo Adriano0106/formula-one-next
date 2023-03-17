232cc9bd89d672b008348c2fe9558c16b903d899
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2023-03-17 04:28:25 -0300

    Estiliza tabela de Equipes e adiciona seletor do ano, além do link do Github no footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import styles from "styles/Footer.module.css";
 import Card from "react-bootstrap/Card";
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <Card className={`${styles.footer} text-center`}>
-      <Card.Footer className={`text-muted`}>Desenvolvido por: Adriano Andrade da Silva</Card.Footer>
+      <Card.Footer className={`text-muted`}>
+        Desenvolvido por:
+        <Link href="https://github.com/Adriano0106">
+          <a target="_blank">&nbsp;Adriano Andrade da Silva &nbsp;<FaGithub/></a>
+        </Link>
+      </Card.Footer>
     </Card>
   );
 };
--- pages/teams.tsx
+++ pages/teams.tsx
@@ -5,26 +5,49 @@ import Team from "components/Team";
 import Request from "components/Request";
 import styled from "styled-components";
 import { TeamInfo } from "interfaces/interfaces";
-
-const Table = styled.table`
-  display: inline-table;
-`;
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Teams: NextPage = () => {
+  var yearSearch = new Date().getFullYear();
+  const [year, setYear] = useState(yearSearch);
   const [teamList, setTeamList] = useState(Array<TeamInfo>);
 
   useEffect(() => {
     const loadAll = async () => {
-      let list = await Request.teamsList(2022);
+      let list = await Request.teamsList(year);
       let data = list[0].items;
       setTeamList(data);
     };
 
     loadAll();
-  }, []);
+  }, [year]);
+
+  const reloadNewYear = (newYear: any) => {
+    setYear(newYear);
+  }
 
   return (
-    <Table className="table">
+    <Container>
+      <Form.Select
+        value={year}
+        onChange={(e: any) => reloadNewYear(e.currentTarget.value)}
+      >
+        <option value="2023">2023</option>
+        <option value="2022">2022</option>
+        <option value="2021">2021</option>
+        <option value="2020">2020</option>
+        <option value="2019">2019</option>
+        <option value="2018">2018</option>
+        <option value="2017">2017</option>
+        <option value="2016">2016</option>
+        <option value="2015">2015</option>
+        <option value="2014">2014</option>
+      </Form.Select>
+      <Table responsive striped bordered hover size="sm">
         <thead>
           <tr>
             <th>Nome</th>
@@ -40,6 +63,7 @@ const Teams: NextPage = () => {
           })}
         </tbody>
       </Table>
+    </Container>
   );
 }
 
