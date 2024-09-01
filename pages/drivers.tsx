d423a06c6648d0e6b7db267994d91cd0239ec182
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2024-08-31 21:22:33 -0300

    Trazer o ano de 2024 por padrão na página de pilotos

--- pages/drivers.tsx
+++ pages/drivers.tsx
@@ -2,17 +2,10 @@ import type { NextPage } from "next";
 import React, { useEffect, useState } from "react";
 import Driver from "components/Driver";
 import Request from "components/Request";
-import styled from "styled-components";
 import { NewDriverInfo } from "interfaces/interfaces";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-// const Table = styled.table`
-//   display: inline-table;
-// `;
 
 let order = "desc";
 
@@ -23,7 +16,6 @@ const Drivers: NextPage = () => {
 
   useEffect(() => {
     const loadAll = async () => {
-      // let list = await Request.driversList(year);
       let list = await Request.newDriversList(year);
       let data = list[0].items;
       setDriverList(data);
@@ -66,6 +58,7 @@ const Drivers: NextPage = () => {
         value={year}
         onChange={(e: any) => reloadNewYear(e.currentTarget.value)}
       >
+        <option value="2024">2024</option>
         <option value="2023">2023</option>
         <option value="2022">2022</option>
         <option value="2021">2021</option>
