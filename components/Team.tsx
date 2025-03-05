cdcd346f7ceda142e66936953252ca961ae7b812
Adriano Andrade <hey.adriano.dev@outlook.com>
2025-03-04 22:45:12 -0300

    Lista dados com base na nova API e adiciona tratativa de erro para equipes

--- components/Team.tsx
+++ components/Team.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import styles from "styles/Team.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
-import { TeamsInfo } from "interfaces/interfaces";
+import { NewTeamsInfo } from "interfaces/interfaces";
 
-const Team = (team: TeamsInfo) => {
+const Team = (team: NewTeamsInfo) => {
   return (
     <tr className={styles.team}>
       <td>{team?.items?.name}</td>
       <td>{team?.items?.nationality}</td>
       <td>
-        <Link href={team?.items?.url}>
+        <Link href={team?.items?.wikipediaPage}>
           <a>
             <FontAwesomeIcon icon={faExternalLink} />
           </a>
--- pages/teams.tsx
+++ pages/teams.tsx
@@ -1,24 +1,21 @@
 import type { NextPage } from "next";
 import React, { useEffect, useState } from "react";
-import Link from "next/link";
 import Team from "components/Team";
 import Request from "components/Request";
-import styled from "styled-components";
-import { TeamInfo } from "interfaces/interfaces";
+import { NewTeamInfo } from "interfaces/interfaces";
 import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 const Teams: NextPage = () => {
-  var yearSearch = new Date().getFullYear();
+  // var yearSearch = new Date().getFullYear();
+  var yearSearch = 2024;
   const [year, setYear] = useState(yearSearch);
-  const [teamList, setTeamList] = useState(Array<TeamInfo>);
+  const [teamList, setTeamList] = useState(Array<NewTeamInfo>);
 
   useEffect(() => {
     const loadAll = async () => {
-      let list = await Request.teamsList(year);
+      let list = await Request.newTeamsList(year);
       let data = list[0].items;
       setTeamList(data);
     };
@@ -30,12 +27,17 @@ const Teams: NextPage = () => {
     setYear(newYear);
   }
 
+  console.log(teamList)
+
   return (
     <Container>
       <Form.Select
         value={year}
         onChange={(e: any) => reloadNewYear(e.currentTarget.value)}
       >
+        <option value="2025">2025</option>
+        <option value="2024">2024</option>
+        {/*
         <option value="2023">2023</option>
         <option value="2022">2022</option>
         <option value="2021">2021</option>
@@ -46,24 +48,28 @@ const Teams: NextPage = () => {
         <option value="2016">2016</option>
         <option value="2015">2015</option>
         <option value="2014">2014</option>
+        */}
       </Form.Select>
       <br />
-      <Table responsive striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Nome</th>
-            <th>Nacionalidade</th>
-            <th>Página</th>
-          </tr>
-        </thead>
-        <tbody className="drivers--list">
-          {teamList.map((item: TeamInfo, index: Number) => {
-            return (
-              <Team key={Number(index)} items={item} />
-            )
-          })}
-        </tbody>
-      </Table>
+      {teamList?.length > 0 ? (
+        <Table responsive striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Nacionalidade</th>
+              <th>Página</th>
+            </tr>
+          </thead>
+          <tbody className="drivers--list">
+            {teamList.map((item: NewTeamInfo, index: number) => (
+              <Team key={index} items={item} />
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <p>{"Nenhuma equipe encontrada"}</p>
+      )}
+
     </Container>
   );
 }
