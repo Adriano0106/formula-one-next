c3691e1d693c46f95d67eda0197f8cb6709b841a
Adriano Andrade <hey.adriano.dev@outlook.com>
2025-03-04 22:43:30 -0300

    Remove rotas ergastAPI e altera para nova API

--- components/Request.tsx
+++ components/Request.tsx
@@ -1,32 +1,13 @@
-const API_BASE = "https://ergast.com/api/f1";
 const NEW_API_BASE = process.env.NEXT_PUBLIC_API_URL;
-
-const requestAPI = async (endpoint: String, type: String) => {
-  const req = await fetch(`${API_BASE}${endpoint}`);
-  const json = await req.json();
-
-  if (type == "Drivers" && json) {
-    return json.MRData.DriverTable.Drivers;
-  }
-
-  if (type == "Teams" && json) {
-    return json.MRData.ConstructorTable.Constructors;
-  }
-
-  if (type == "Drivers Champions" && json) {
-    return json.MRData.StandingsTable.StandingsLists;
-  }
-
-  if (type == "Teams Champions" && json) {
-    return json.MRData.StandingsTable.StandingsLists;
-  }
-};
 
 const newRequestAPI = async (endpoint: String, type: String) => {
   const req = await fetch(`${NEW_API_BASE}${endpoint}`);
   const json = await req.json();
 
   if (type == "Drivers" && json) {
+    return json;
+  }
+  if (type == "Teams" && json) {
     return json;
   }
   if (type == "Drivers Champions" && json) {
@@ -38,39 +19,12 @@ const newRequestAPI = async (endpoint: String, type: String) => {
 };
 
 const requests = {
-  driversList: async (year: Number) => {
-    return [
-      {
-        slug: "drivers",
-        title: `Formula One Drivers ${year}`,
-        items: await newRequestAPI(`/${year}/drivers.json`, "NewDrivers"),
-      },
-    ];
-  },
-  teamsList: async (year: Number) => {
+  newTeamsList: async (year: Number) => {
     return [
       {
         slug: "teams",
         title: `Formula One Teams ${year}`,
-        items: await requestAPI(`/${year}/constructors.json`, "Teams"),
-      },
-    ];
-  },
-  driversChampionsList: async () => {
-    return [
-      {
-        slug: "driversChampions",
-        title: `Formula One Drivers Champions`,
-        items: await requestAPI(`/driverStandings/1.json?limit=100`, "Drivers Champions"),
-      },
-    ];
-  },
-  teamsChampionsList: async () => {
-    return [
-      {
-        slug: "teamsChampions",
-        title: `Formula One Teams Champions`,
-        items: await requestAPI(`/constructorStandings/1.json?limit=100`, "Teams Champions"),
+        items: await newRequestAPI(`/teams/${year}`, "Teams"),
       },
     ];
   },
@@ -83,7 +37,6 @@ const requests = {
       },
     ];
   },
-
   newDriversChampionsList: async () => {
     return [
       {
@@ -93,7 +46,6 @@ const requests = {
       },
     ];
   },
-
   newTeamsChampionsList: async () => {
     return [
       {
